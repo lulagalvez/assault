eb8434f12f2b0d6e60d1ca826e746229a10cad39
Lula Galvez <luisgalvezuk@gmail.com>
2023-07-11 16:32:27 -0400

    changes

--- vectorizedmodel.py
+++ vectorizedmodel.py
@@ -6,9 +6,9 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_util import make_atari_env
 import os
 
-learning_rate = 0.00025
+learning_rate = 0.0007
 gamma = 0.99
-ent_coef = 0.1
+ent_coef = 0.01
 
 
 env = make_atari_env("ALE/Assault-v5", n_envs=4, seed=0)
@@ -17,21 +17,17 @@ env = VecFrameStack(env, n_stack=4)
 
 log_path = os.path.join('Training', 'Logs', 'new_logs')
 
-model = PPO('CnnPolicy', env,
+model = A2C('CnnPolicy', env,
             verbose=1,
             tensorboard_log=log_path,
             learning_rate=learning_rate,
             gamma=gamma,
             ent_coef=ent_coef,
-            n_epochs=4,
-            batch_size=32,
-            vf_coef=1,
-            gae_lambda=0.95,
-            clip_range=0.5
+            n_steps=5,
             )
 
-model.learn(total_timesteps=1000000) 
+model.learn(total_timesteps=400000) 
 
-model_path = os.path.join('Training', 'Saved Models', 'ppo', 'PPO_LR00025_1M')
+model_path = os.path.join('Training', 'Saved Models', 'a2c', 'LR0007_EC001_400k')
 
 model.save(model_path)
